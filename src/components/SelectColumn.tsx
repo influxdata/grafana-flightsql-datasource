e128d2064bb65b64fa5c6045eebea79d16ce8c2d
helenosheaa <38860767+helenosheaa@users.noreply.github.com>
2023-01-12 16:38:25 -0500

    feat: select column component

---
+++ src/components/SelectColumn.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Select } from '@grafana/ui';
+
+export const SelectColumn = ({columns, index, setColumn, column, formatCreateLabel}: any) => {
+    columns && columns.forEach((c: any) => c.index = index)
+    return (
+        <Select
+        options={columns}
+        onChange={setColumn}
+        // isLoading={loadingColumn}
+        // disabled={!!errorColumn}
+        key={index}
+        value={column.value}
+        allowCustomValue={true}
+        autoFocus={true}
+        formatCreateLabel={formatCreateLabel}
+        width={30}
+        placeholder=""
+      />
+ )}
+   
