2da4fd69f454e1ad0ce7c6ff2e90593a3c480713
Brett Buddin <brett@buddin.org>
2023-01-13 14:00:48 -0500

    fix: Exclude linux ARM from the build list, because Arrow doesn't support it in the matrix.

--- Magefile.go
+++ Magefile.go
@@ -6,7 +6,15 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
-// Default configures the default target.
-var Default = build.BuildAll
+// BuildAll is copied from Grafana's SDK (build package), because the Arrow
+// dependency doesn't support Linux ARM in the build matrix. I've removed it
+// from the listing here.
+func BuildIt() {
+	b := build.Build{}
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64)
+}
+
+var Default = BuildIt
