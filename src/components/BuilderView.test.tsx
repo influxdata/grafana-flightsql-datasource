d9a8ed562607c5b1b0a469aeb92ecb237a8f3689
helenosheaa <38860767+helenosheaa@users.noreply.github.com>
2023-02-02 15:50:31 -0500

    feat: render builder view

---
+++ src/components/BuilderView.test.tsx
@@ -0,0 +1,44 @@
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+
+import {BuilderView} from './BuilderView'
+
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
+
+const setup = async (optionOverrides?: object) => {
+  const props = {
+    query: '',
+    datasource: '',
+    onChange: () => {},
+    fromRawSql: true,
+  }
+
+  await render(<BuilderView {...props} />)
+}
+
+describe('BuilderView', () => {
+  it('should render without throwing an error', () => {
+    expect(() => setup()).not.toThrow()
+  })
+  it('should show the default form elements', () => {
+    setup()
+    expect(screen.getByText(/from/i)).toBeInTheDocument()
+    // expect(screen.getByText(/select/i)).toBeInTheDocument()
+    expect(screen.getByText(/where/i)).toBeInTheDocument()
+    expect(screen.getByText(/group by/i)).toBeInTheDocument()
+    expect(screen.getByText(/order by/i)).toBeInTheDocument()
+    expect(screen.getByText(/limit/i)).toBeInTheDocument()
+  })
+})
